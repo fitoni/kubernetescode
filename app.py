435627048f6e2219d4f39f05bdb2f7908cb9bc10
fitoni no <fitoni77@gmail.com>
2023-02-12 22:04:57 +0700

    updated returned message #2

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is data returned from Flask Web Server!'
+    return 'This is data returned from python-flask Web Server!'
